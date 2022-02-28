571930a9700e0b50d2f12cc52e2d3e29c5ec8026
fenteale <mangmasta@gmail.com>
2022-02-28 14:41:19 -0800

    removed original package dirs

--- dev_ws/src/rmc_camera/rmc_camera/detector/obstacle.py
+++
@@ -1,9 +0,0 @@
-class Obstacle:
-    def __int__(self, cv):
-        self.cv = cv
-
-    def detect(self):
-        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return self.cv.Canny(imgGray)
-
-        return
